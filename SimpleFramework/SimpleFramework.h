998a702bc502ed17a5c8f7af6b14e8da44ba65e1
Anil Arigela <anil.a@peopleinteractive.in>
2020-01-07 11:08:02 +0530

    Initial Commit

---
+++ SimpleFramework/SimpleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimpleFramework.h
+//  SimpleFramework
+//
+//  Created by Anil Arigela on 07/01/20.
+//  Copyright © 2020 Anil Arigela. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleFramework.
+FOUNDATION_EXPORT double SimpleFrameworkVersionNumber;
+
+//! Project version string for SimpleFramework.
+FOUNDATION_EXPORT const unsigned char SimpleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleFramework/PublicHeader.h>
+
+
